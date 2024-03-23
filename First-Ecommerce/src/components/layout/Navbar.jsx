ab4f4e33d0f1ede19918b5530185f263d08e9c01
Agustin <agusslb@outlook.com>
2024-03-23 13:09:48 -0300

    agregado de rutas

--- First-Ecommerce/src/components/layout/Navbar.jsx
+++ First-Ecommerce/src/components/layout/Navbar.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 import CartWidget from "../common/CartWidget";
 import "./Navbar.css";
+import { menuNavigation } from "../../Router/menuNavegation";
 
 export const Navbar = () => {
   return (
     <>
       <div className="containerNavbar">
-        <Link to="/" style={{ color: "beige" }}>
+        <Link to="/" style={{ color: "withe" }}>
           BlueBird
         </Link>
 
         <ul className="categories">
-          <Link to="/">Todas</Link>
-          <Link to="/category/Teclados">Teclados</Link>
-          <Link to="/category/Mouse">Mouse</Link>
+          {menuNavigation.map(({ id, text, path }) => (
+            <Link key={id} to={path}>
+              {text}
+            </Link>
+          ))}
         </ul>
         <CartWidget />
       </div>
--- First-Ecommerce/src/context/CartContext.jsx
+++ First-Ecommerce/src/context/CartContext.jsx
@@ -3,7 +3,10 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
-  const [cart, setCart] = useState([]); // [ ]---> {} ---> [{1}] ---> [{1}, {3}]
+  // cart ---> [{}, {}]
+  const [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
 
   const addToCart = (product) => {
     let existe = isInCart(product.id);
@@ -21,13 +24,16 @@ const CartContextProvider = ({ children }) => {
       });
 
       setCart(newArray);
+      localStorage.setItem("cart", JSON.stringify(newArray));
     } else {
       setCart([...cart, product]);
+      localStorage.setItem("cart", JSON.stringify([...cart, product]));
     }
   };
 
   const clearCart = () => {
     setCart([]);
+    localStorage.removeItem("cart");
   };
 
   const isInCart = (id) => {
@@ -36,8 +42,9 @@ const CartContextProvider = ({ children }) => {
   };
 
   const removeById = (id) => {
-    let newArray = cart.filter((elemento) => elemento.id !== id); // ---> []
+    let newArray = cart.filter((elemento) => elemento.id !== id);
     setCart(newArray);
+    localStorage.setItem("cart", JSON.stringify(newArray));
   };
 
   const getTotalItems = () => {
@@ -56,6 +63,7 @@ const CartContextProvider = ({ children }) => {
   };
 
   const getTotalQuantityById = (id) => {
+    // si encuentra devuelve el elemento, si no, undefined
     let product = cart.find((elemento) => elemento.id === id);
 
     if (product) {
