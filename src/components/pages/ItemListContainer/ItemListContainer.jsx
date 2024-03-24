cfcaeecd800a5aabb5f9877a58e81602b8950048
Agustin <agusslb@outlook.com>
2024-03-23 22:00:05 -0300

    agregado de rutas

--- src/components/pages/ItemListContainer/ItemListContainer.jsx
+++ src/components/pages/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import ItemList from "./Itemlist";
+import ItemList from "./ItemList";
 import "./ItemListContainer.css";
 import { db } from "../../../fireBaseConfig";
 import { collection, getDocs, query, where } from "firebase/firestore";
